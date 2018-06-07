fee1e7b907741f17ee5a16b99e2fb60ae35fd460
Chris Grocott <chris.grocott@gmail.com>
2018-06-07 14:10:46 +0100

    Added another consumer to work queue example

--- src/main/scala/workqueue/repl/MainLoop.scala
+++ src/main/scala/workqueue/repl/MainLoop.scala
@@ -6,18 +6,22 @@ import scala.io.StdIn._
 
 object MainLoop extends App {
 
-  val consumer: SlowConsumer = new SlowConsumer("Drone")
+  val consumer1: SlowConsumer = new SlowConsumer("Drone 1")
+  val consumer2: SlowConsumer = new SlowConsumer("Drone 2")
 
-  val consumerDaemon = new Thread(consumer)
+  val consumerDaemon1 = new Thread(consumer1)
+  val consumerDaemon2 = new Thread(consumer2)
 
-  consumerDaemon.start
+  consumerDaemon1.start
+  consumerDaemon2.start
 
   def handleMessage(msg: String): Unit = BasicPublisher.publishMessage(msg)
 
   def loop: Unit = {
     val message = readLine("What do you want to say?: ")
     if (message equals ":q") {
-      consumer.blat
+      consumer1.blat
+      consumer2.blat
     } else {
       handleMessage(message)
       loop
