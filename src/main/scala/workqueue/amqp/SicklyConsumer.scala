b3d4ed129d45835e9223f0b32e621c33ef3fb399
Chris Grocott <chris.grocott@gmail.com>
2018-06-07 14:52:35 +0100

    added a sickly consumer to see rabbit requeuing messages

---
+++ src/main/scala/workqueue/amqp/SicklyConsumer.scala
@@ -0,0 +1,44 @@
+package workqueue.amqp
+
+import com.rabbitmq.client._
+
+// receives a message then kicks the bucket without acking, just want to see rabbit requeue the messages!
+
+class SicklyConsumer(_name: String) extends Runnable {
+
+  val name = _name
+
+  val QUEUE_NAME: String = "hello"
+
+  val factory = new ConnectionFactory
+  factory.setHost("localhost")
+  val connection = factory.newConnection
+  val channel = connection.createChannel
+
+  channel.queueDeclare(QUEUE_NAME, false, false, false, null)
+
+  override def run(): Unit = {
+
+    println(s" [*] Consumer $name waiting for messages (*cough cough*).")
+
+    val consumer: Consumer = new DefaultConsumer(channel) {
+
+      override def handleDelivery(consumerTag: String, envelope: Envelope, properties: AMQP.BasicProperties, body: Array[Byte]) = {
+        val message: String = new String(body, "UTF-8")
+        println(s" [x] $name received '$message', processing...")
+        println(s" [x] $name doesn't feel so good... urrrrghhh")
+        channel.close()
+        connection.close()
+      }
+    }
+
+    channel.basicConsume(QUEUE_NAME, false, consumer)
+
+  }
+
+  def blat: Unit = {
+
+    channel.close()
+    connection.close()
+  }
+}
--- src/main/scala/workqueue/repl/MainLoop.scala
+++ src/main/scala/workqueue/repl/MainLoop.scala
@@ -1,19 +1,25 @@
 package workqueue.repl
 
-import workqueue.amqp.{SlowConsumer, BasicPublisher}
+import workqueue.amqp.{BasicPublisher, SicklyConsumer, SlowConsumer}
 
 import scala.io.StdIn._
 
 object MainLoop extends App {
 
+  val sicklyConsumer: SicklyConsumer = new SicklyConsumer("Darren Anderton")
   val consumer1: SlowConsumer = new SlowConsumer("Drone 1")
   val consumer2: SlowConsumer = new SlowConsumer("Drone 2")
+  val consumer3: SlowConsumer = new SlowConsumer("Drone 3")
 
+  val sicklyDaemon = new Thread(sicklyConsumer)
   val consumerDaemon1 = new Thread(consumer1)
   val consumerDaemon2 = new Thread(consumer2)
+  val consumerDaemon3 = new Thread(consumer3)
 
-  consumerDaemon1.start
-  consumerDaemon2.start
+  sicklyDaemon.start()
+  consumerDaemon1.start()
+  consumerDaemon2.start()
+  consumerDaemon3.start()
 
   def handleMessage(msg: String): Unit = BasicPublisher.publishMessage(msg)
 
@@ -22,6 +28,7 @@ object MainLoop extends App {
     if (message equals ":q") {
       consumer1.blat
       consumer2.blat
+      consumer3.blat
     } else {
       handleMessage(message)
       loop
